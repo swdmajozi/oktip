17d34ba7af7f29edb80cfb4cd9855f9b6445c95b
Siyandam <Siyandam@DESKTOP-BLEA77C>
2021-07-15 05:14:54 +0200

    first commit

--- config/nexmo.php
+++
@@ -1,6 +0,0 @@
-<?php  (! defined('BASEPATH')) and exit('No direct script access allowed');
-// please get the following api on Nexmo Dashboard.
-$config['api_key'] = 'a8f4550c';
-$config['api_secret'] = 'ADzdvKx5QqnmV49C';
-/* End of file nexmo.php */
-/* Location: ./application/config/nexmo.php */
